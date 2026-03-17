9d7852270972790fb71c93d7ad8496385d88c827
student <student@itismeucci.com>
2026-03-17 12:16:50 +0100

    database fatto manca insert

--- lamp-slim-react/build/init.sql
+++ lamp-slim-react/build/init.sql
@@ -1,25 +1,22 @@
-CREATE TABLE `alunni` (
+CREATE TABLE `accounts` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(20) NOT NULL,
-  `cognome` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-CREATE TABLE `certificazioni` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `alunno_id` INT(11) NOT NULL,
-  `titolo` VARCHAR(100) NOT NULL,
-  `votazione` INT(3) NOT NULL CHECK (`votazione` BETWEEN 0 AND 100),
-  `ente` VARCHAR(100) NOT NULL,
+  `owner_name` VARCHAR(255) NOT NULL,
+  `currency` VARCHAR(255) NOT NULL,
+  `balance` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`alunno_id`) REFERENCES `alunni`(`id`) ON DELETE CASCADE
+   `created_at` DATE NOT NULL,
+   check ( balance > 0)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `alunni` (`nome`, `cognome`) VALUES
-('claudio', 'benve'),
-('ivan', 'bruno');
+CREATE TABLE `transactions` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `account_id` INT(11) NOT NULL,
+  `type` VARCHAR(255) NOT NULL,
+  `amount` INT(11) NOT NULL,
+  `description` VARCHAR(255) NOT NULL,
+    `created_at` DATE NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`account_id`) REFERENCES `accounts`(`id`) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `certificazioni` (`alunno_id`, `titolo`, `votazione`, `ente`) VALUES
-(1, 'Certificazione Python', 85, 'Coursera'),
-(1, 'Certificazione SQL', 90, 'Udemy'),
-(2, 'Certificazione Java', 78, 'Oracle');
+
